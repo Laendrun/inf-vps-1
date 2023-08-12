b6745dd2335955a08f24438acb11a86fe665ca25
Simon <simon.aeby@laendrun.ch>
2023-08-12 17:08:39 +0200

    feat:#17 add error handlers;

--- telegram_bot/index.js
+++ telegram_bot/index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const middleware = require('./middleware');
 
 const app = express();
 
 const bot = require('./bot');
 
+console.log(process.env.NODE_ENV);
+
 app.use(cors());
-
 app.use(express.json());
-
 app.use(logger('common'));
-
 app.use('/', bot);
+
+app.use(middleware.notFound);
+app.use(middleware.errorHandler);
 
 const port = process.env.PORT || 4000;
 
