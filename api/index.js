abd419b59bab73341379bb2b6934efd6fb9fda0a
Simon <simon.aeby@laendrun.ch>
2023-09-09 17:08:58 +0200

    feat: add Swiss QR Bill generation endpoint; rename api container

--- api/index.js
+++ api/index.js
@@ -4,14 +4,20 @@ const db = monk(process.env.DB_URL)
 
 const app = express();
 
+app.use(express.json())
+
+const v2 = require('./v2/index.js');
+
 app.get("/", (req, res, next) => {
-	const users = db.get('users');
-	users.insert({woot:'foo'}, {woot:'bar'});
+	// const users = db.get('users');
+	// users.insert({woot:'foo'}, {woot:'bar'});
 
 	res.json({
 		"message": "success"
 	});
 });
+
+app.use("/v2/", v2);
 
 const notFound = (req, res, next) => {
 	res.status(404);
@@ -19,7 +25,18 @@ const notFound = (req, res, next) => {
 	next(error);
 }
 
+const errorHandler = (err, req, res, next) => {
+	if (res.headerSent)
+		return next(err);
+	console.error(err)
+	res.json({
+		message: err.message,
+		path: req.originalUrl
+	})
+}
+
 app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 4002
 const host = '0.0.0.0'
